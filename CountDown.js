d8f1b2d0d6d4ac441104c7b4dc93f87c8eef3a84
Daon1109 <86394944+Daon1109@users.noreply.github.com>
2021-09-19 08:23:18 +0900

    Update CountDown.js

--- CountDown.js
+++ CountDown.js
@@ -10,6 +10,10 @@ var x = setInterval(function() {
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
+  //loop
+  var i;
+  var timeArray = document.querySelectorAll('.timeleft');
+
   //testcode (a day = 86400000 , an year = 31536000000)
   //var test = new Date("Sep 19, 2022 00:00:00").getTime();
   //var distanceTest = countDownDate - test;
@@ -23,20 +27,26 @@ var x = setInterval(function() {
 
   // Display the result in the element with id="countdown"
   if(0 < distance)  {
-  document.getElementById("countdown").innerHTML = days + " Day " + hours + " Hour "
-  + minutes + " Minutes " + seconds + " Seconds ";
+  document.getElementById("day").innerHTML = days + " Day ";
+  document.getElementById("hour").innerHTML = hours + " Hour ";
+  document.getElementById("min").innerHTML = minutes + " Minute ";
+  document.getElementById("sec").innerHTML = seconds + " Second ";
   document.getElementById("title").innerHTML = "XXX 생일 카운트다운";
   document.getElementById("leftalign").innerHTML = "***";
   document.getElementById("rightalign").innerHTML = "***";
     if(distance < 11000) {
       document.getElementById("emphasize").innerHTML = seconds;
+      for(i=0; i<timeArray.length; i++) {
+          timeArray[i].style.display="none";
+      }
     }
   }
 
   // Count down is finished
   else if (-86400000 < distance && distance < 0) {
+    document.querySelector(".timeleft").innerHTML = " ";
     document.getElementById("countdown").innerHTML = "Today is your birthday!<br>Happy Birthday!";
-    document.getElementById("title").innerHTML = "생일 ㅊㅋㅊㅋ";
+    document.getElementById("title").innerHTML = "생일 ㅊㅋ";
     document.getElementById("leftalign").innerHTML = "경";
     document.getElementById("rightalign").innerHTML = "축";
     document.getElementById("emphasize").innerHTML = " ";
