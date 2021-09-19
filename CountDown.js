93ce04c7b3bde4f895c9033576c0f64d92ad9bb9
Daon1109 <86394944+Daon1109@users.noreply.github.com>
2021-09-20 06:08:32 +0900

    countdown errorfix

--- CountDown.js
+++ CountDown.js
@@ -1,5 +1,5 @@
 // Set the date program will be counting down to
-var countDownDate = new Date("Jun 11, 2021 00:00:00").getTime();
+var countDownDate = new Date("Jun 11, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
